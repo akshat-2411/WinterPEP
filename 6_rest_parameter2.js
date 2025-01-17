20c0ef544f0ddb01bc73f2078a626b84fa95e4e6
Akshat Sharma <119062407+santoshshanu@users.noreply.github.com>
2025-01-17 09:58:39 +0530

    Delete 6_rest_parameter2.js

--- 6_rest_parameter2.js
+++
@@ -1,7 +0,0 @@
-function myFun(a,b,...manyMoreArgs){
-    console.log("a",a);
-    console.log("b",b);
-    console.log("manyMoreArgs",manyMoreArgs);
-}
-
-myFun("one","two","three","four","five","six")
