cf38b61e31db3fae16fc7c26799a8cf21f88e5e8
Akshat Sharma <119062407+santoshshanu@users.noreply.github.com>
2025-01-17 09:59:39 +0530

    Delete Q3.js

--- Q3.js
+++
@@ -1,7 +0,0 @@
-function logArguments(a,b,...manyMoreArgs){
-    console.log("a",a);
-    console.log("b",b);
-    console.log("manyMoreArgs",manyMoreArgs);
-}
-
-logArguments("1","2","3","4","5","6","7")
